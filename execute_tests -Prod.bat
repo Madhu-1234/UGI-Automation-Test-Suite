6482f7b2c83a85539c6f3db82f5439698de13dbd
Madhu-1234 <36332967+Madhu-1234@users.noreply.github.com>
2020-10-09 00:16:35 +0530

    Update execute_tests -Prod.bat

--- execute_tests -Prod.bat
+++ execute_tests -Prod.bat
@@ -2,5 +2,4 @@ npm run test-prod & ^
 allure generate allure-results-Prod --clean & ^
 xcopy /s /e /v /y "C:\Automated Testing\UGI-Automation-Test-Suite\allure-report" "\\aisuetst-flb002\C$\inetpub\wwwroot\UGIAutomationTestReport" & ^
 node utils/sendmail.js & ^
-npm run allure-report & ^
 PAUSE
