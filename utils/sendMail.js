87fa421cc2b4e672c31a83abc4055e886553196c
alashkarwala <abdulkader.lashkarwala@mottmac.com>
2020-10-08 09:47:16 -0400

    Modified SendMail.js to add my email address and modified the bat file to copy the report after execution to our server instead of Netlify

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -7,11 +7,11 @@ const fs = require("fs");
 //attachment = fs.readFileSync(pathToAttachment).toString("base64");
 
 const msg = {
-  to: ['madhu@shrisaitechnology.com', 'madhusmita.m12@gmail.com','Anushree.Pradhan@mottmac.com'],
+  to: ['madhu@shrisaitechnology.com', 'madhusmita.m12@gmail.com','Anushree.Pradhan@mottmac.com', 'AbdulKader.Lashkarwala@mottmac.com'],
   from: 'fbsupport@mottmac.com', // Use the email address or domain you verified above
   subject: 'UGI Automation test run results',
   text: 'Link for UGI Automation test results',
-  html: '<strong>https://admiring-chandrasekhar-99ed2e.netlify.app/</strong>',
+  html: 'Automated test suite has been executed successfully and report has been uploaded to - <strong>https://testshowcase.mottmac.com/UGIAutomationTestReport/</strong>',
   
     
   
