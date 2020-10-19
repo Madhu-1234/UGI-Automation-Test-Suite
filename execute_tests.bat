fda530442500875a5f515d9c45db9e0378a08c60
Madhu-1234 <36332967+Madhu-1234@users.noreply.github.com>
2020-10-20 00:31:27 +0530

    Updated to add allure generate

--- execute_tests.bat
+++ execute_tests.bat
@@ -1,6 +1,7 @@
 
 npm run cleandir & ^
 npm run test & ^
+allure generate allure-results & ^
 xcopy /s /e /v /y "C:\Automated Testing\UGI-Automation-Test-Suite\allure-report" "\\aisuetst-flb002\C$\inetpub\wwwroot\UGIAutomationTestReport" & ^
 node utils/sendmail.js
 REM npm run allure-report & ^
